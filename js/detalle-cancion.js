2df1700cef45f1a8efe6977a9ba76053ba1e51c5
Bonann <111580049+Bonann@users.noreply.github.com>
2023-06-08 23:30:58 -0300

    detalles de cancion

--- js/detalle-cancion.js
+++ js/detalle-cancion.js
@@ -1,3 +1,30 @@
 console.log("Bien vinculado el Detalle de una cancion");
 
 let urlDetalleCancion = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/${id}`
+
+fetch(urlDetalleCancion)
+.then(function(response) {
+  return response.json()
+})
+.then(function(data) {
+  console.log(data);
+  let info = info.data;
+  let canciones = document.querySelector("#detalles");
+  let detalles = ""
+
+  detalles += ` <article class="la-info"> 
+              <img src="${info[i].album.cover}" alt=" " >
+              <h4> Title: ${info[i].title}   </h4>
+              <p> Compuesta por: ${info[i].artist.name}  </p>
+              <p> Duracion: ${info[i].duration}  </p>
+              <p> Parte del album: ${info[i].album.title}  </p>
+              <p> Se encuentra en el top: ${info[i].position}  </p>
+               </article>
+              `
+              canciones.innerHTML = detalles;
+
+})
+.catch(function(error) {
+  console.log("Error: " + error);
+})
+
