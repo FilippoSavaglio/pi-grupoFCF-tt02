0e0bce6cd98140eb733b13eb8a6c9255f3ee122c
cirilo2 <cbullrich@udesa.edu.ar>
2023-06-09 17:42:38 -0300

    buscador inc

--- js/resultado-busqueda.js
+++ js/resultado-busqueda.js
@@ -1,3 +1,19 @@
-console.log("Bien vinculado el resultado busqueda");
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(querystring);
+let formulario = queryStringObj.get("search");
+
+let datoBuscado = document.querySelector(".result-titulo")
 
 let urlBuscador = `https://api.allorigins.win/raw?url=https://api.deezer.com/search?q=${formulario}`
+
+fetch(urlBuscador)
+.then(function(result){
+    return response.json();
+})
+.then(function(data){
+
+console.log(data);
+
+
+
+})
