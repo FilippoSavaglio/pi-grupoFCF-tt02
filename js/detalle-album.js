3356620d90be64c2358d051e95e90119675995a7
Bonann <111580049+Bonann@users.noreply.github.com>
2023-06-12 10:33:23 -0300

    detalles album

--- js/detalle-album.js
+++ js/detalle-album.js
@@ -11,19 +11,19 @@ fetch(urlDetalleAlbum)
 .then (function (data){
     console.log(data);
 
-    let nombreAlbum = document.querySelector('.nombre');
+    let nombreAlbum = document.querySelector('.titulo-detalles');
     nombreAlbum.innerText += `${data.title}`
 
-    let imagenAlbum = document.querySelector('.foto-art');
-    imagenAlbum.innerHTML += `<img with = "400px" class= "detalle" src="${data.cover}" alt="foto del album">`
+    let imagenAlbum = document.querySelector('.img');
+    imagenAlbum.innerHTML += `<img with = "400px" class= "detalle-foto" src="${data.cover_big}" alt="foto del album">`
 
-    let ContArt = document.querySelector('.name');
+    let ContArt = document.querySelector('.h4-artista');
     ContArt.innerHTML += `<a href="detalle-artista.html?id=${data.artist.id}">${data.artist.name}</a>`
 
-    let genero = document.querySelector('.genero');
-    genero.innerHTML += `falta la pag de detalle de genero `
+    let genero = document.querySelector('.h4_album');
+    genero.innerHTML += `<a href="detalle-genero.html?id=${data.genre_id}">${data.genre.data[0].name}</a>`
 
-    let date = document.querySelector('.fecha');
+    let date = document.querySelector('.h4-release');
     date.innerHTML += `${data.release_date}`
 
 })
@@ -32,4 +32,62 @@ fetch(urlDetalleAlbum)
 })
 
 let urlDetalleCancion = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/${id}`
+
+fetch(urlDetalleCancion)
+.then(function (response) {
+    return response.json();
+})
+.then(function(data){
+    console.log(data);
+    let arrayInfo = data.data;
+    let tracklist = document.querySelector(".lista1");
+    let contenidoLista ="";
+    console.log(data);
+
+    for (let i = 0; i < arrayInfo.length; i++){
+        contenidoLista += `
+              <li class="top">
+                 <a href="detalle-cancion.html?id=${arrayInfo[i].id}"><i class="fafa"></i>${arrayInfo[i].title}</a>
+              </li>
+        `
+    }
+         tracklist.innerHTML += contenidoLista;
+
+})
+.catch(function(error){
+    console.log(error);
+})
+ 
+
+let urlDetalleArtista = `https://api.allorigins.win/raw?url=https://api.deezer.com/artist/${id}`;
+
+fetch(urlDetalleArtista)
+.then(function(response){
+    return response.json();
+})
+
+.then(function(data){
+
+    let datos = data.data;
+    let topAlbums = document.querySelector(".artistas");
+    let contenedor = "";
+    console.log(data)
+
+    for(let i=0; i<5; i++){
+        contenedor += `
+        <article class="artista-home">
+        <a href="detalle-album.html?id=${datos[i].id}"><img src="${datos[i].cover_big}" alt"album imagen"></a>
+        <h4><a href="detalle-album.html?id=${datos[i].id}">${datos[i].title}</a></h4>
+        <h4><a class="nombreArtista" href="detalle-artista.html?id=${datos[i].artist.id}">${datos[i].artist.name}</a></h4> 
+        </article>    `
+
+
+    }
+    topAlbums.innerHTML += contenedor;
+})
+
+.catch(function(error){
+    console.log(error);
+})
+
 
