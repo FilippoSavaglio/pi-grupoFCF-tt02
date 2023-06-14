a32de70dbd9dc694f1ad11145aac4065cf86fab0
cirilo2 <cbullrich@udesa.edu.ar>
2023-06-14 15:34:40 -0300

    detalle genero y playlist

--- js/detalle-genero.js
+++ js/detalle-genero.js
@@ -1,3 +1,37 @@
-console.log("Bien vinculado el detalle de genero");
+let queryString = location.search;
+let queryStringToObject = new URLSearchParams(queryString);
+idG = queryStringToObject.get(id)
 
 let urlDetalleGenero = `https://api.allorigins.win/raw?url=https://api.deezer.com/genre/${id}/artists`
+
+fetch(urlDetalleGenero)
+.then(function(response){
+    return response.json()
+})
+.then(function(data) {
+    console.log(data);
+    let arrayI = data.data;
+
+    let nombre = queryStringToObject.get('genre');
+
+    nombreGene = document.querySelector(".genres-detail-titulo");
+    nombreGene.innerText = `Lo mejor del ${nombre}`
+
+    let sectionAlbum = document.querySelector('.generos')
+    let contenidoSection = '';
+
+    for(let i=0; i<arrayI.length; i++){
+        contenidoSection += `
+        <article class="dentro">
+        <a href="detalle-artista.html?id=${arrayI[i].id}"><img src="${arrayI[i].picture_big}" alt="artista imagen"></a>
+        <h4><a href="detalle-artista.html?id={arrayI[i].id}">${arrayI[i].name}</a></h4>
+    </article>
+        `
+    }
+    sectionAlbum.innerText += contenidoSection;
+
+})
+.catch(function(error){
+    console.log(error);
+})
+
--- js/playlist.js
+++ js/playlist.js
@@ -1,19 +1,52 @@
 console.log("Bien vinculado playlist");
 
-let urlPlaylist = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/${id}`;
+let urlPlaylist = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/${favoritos[i]}`;
 
 
 /*                      Recuperar de favoritos    (PROXIMA CLASE)                   */
 
 /* Recupero el storage */
+let recuperoStorage = localStorage.getItem('favoritos');
 
 /* transformar el json (string) en obj o un array */
+let favoritos = JSON.parse(recuperoStorage)
 
 /* Recuperar el elemento del DOM */
+let section = document.querySelector("#lista");
 
 /* Crear personajesFavoritos string vaci para luego ser completado con el fetch */
+let cancionesfav = "";
 
 /* Preguntar: Favoritos es null O su longitus es igual a 0
 TRUE: dar un mensaje en la section diciendo que no hay datos en favoritos
 FALSE: Hacer un FOR que recorra favoritos y haga un fetch por cada elemento del array de favoritos*/
+if (favroitos == null || favoritos.length == 0) {
+    section.innerHTML = "<p>No hay favoritos seleccionados</p>"
+    
+} else {
+    
+    for (let i=0; i < favroitos.length; i++){
+        let urlPlay = `https://api.allorigins.win/raw?url=https://api.deezer.com/track/${favoritos[i]}`
+
+        fetch(urlPlay)
+        .then(function(data){
+            return Response.JSON()
+        })
+        .then(function(data) {
+            console.log(data);
+
+            cancionesfav += `
+            <article class="section-home">  
+                    <a href="detalle-cancion.html?id=${data[i].id}"><img src="${data[i].album.cover_big}" alt=""></a>
+                    <h5><a class="nombreDelArtista" href="detalle-artista.html?id=${data[i].artist.id}">${datos[i].artist.name}</a></h5>
+                    <h3><a href="detalle-cancion.html?id=${data[i].id}">${data[i].title}</a></h3>
+            </article>
+            ` 
+        section.innerHTML = cancionesfav;
+        })
+        .catch(function(error) {
+            console.log(error);
+        })
+    }
+}
     /* No hay favoritos */
